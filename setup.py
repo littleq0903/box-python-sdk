f5e48501b1790f863d2f0849c06a23a8f5a18dab
LittleQ <littleq0903@gmail.com>
2012-06-02 21:24:58 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+""" Box API setup script """
+
+from distutils.core import setup
+
+setup(name='boxapi',
+        version='1.0',
+        description='Access Box.net v2 API from Python',
+        author='Colin Su',
+        author_email='littleq0903@gmail.com',
+        url='https://github.com/littleq0903/box-python-sdk',
+        packages=['boxapi']
+        )
