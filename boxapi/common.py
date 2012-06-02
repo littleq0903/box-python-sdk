13fe9eb3b582ac6e789f47c8072bd374578cea78
LittleQ <littleq0903@gmail.com>
2012-06-02 16:56:31 +0800

    removed the useless variables

--- boxapi/common.py
+++ boxapi/common.py
@@ -6,5 +6,3 @@ __author__ = "Colin Su <littleq0903@gmail.com>"
 API_V1_URL = "https://www.box.com/api/1.0/rest"
 API_V2_URL = "https://www.box.com/api/2.0"
 
-API_KEY = ''
-TICKET_FILE_PATH = '~/.boxapiticket'
